41a19d359473d2c2056725ba0f8900246f329fa5
Muhammad Tawil <muhammad.tawil@hotmail.com>
2022-10-06 19:41:19 +0300

    add login screen

--- authmethods/lib/screens/login_screen.dart
+++ authmethods/lib/screens/login_screen.dart
@@ -17,38 +17,48 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            CustomButton(
-              onTap: () {
-                Navigator.pushNamed(context, EmailPasswordSignup.routeName);
-              },
-              text: 'Email/Password Sign Up',
-            ),
-            CustomButton(
-              onTap: () {
-                Navigator.pushNamed(context, EmailPasswordLogin.routeName);
-              },
-              text: 'Email/Password Login',
-            ),
-            CustomButton(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.3,
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('Auth Methods'),
+                ),
+              ),
+              CustomButton(
                 onTap: () {
-                  Navigator.pushNamed(context, PhoneScreen.routeName);
+                  Navigator.pushNamed(context, EmailPasswordSignup.routeName);
                 },
-                text: 'Phone Sign In'),
-            CustomButton(
-              onTap: () {},
-              text: 'Google Sign In',
-            ),
-            CustomButton(
-              onTap: () {},
-              text: 'Facebook Sign In',
-            ),
-            CustomButton(
-              onTap: () {},
-              text: 'Anonymous Sign In',
-            ),
-          ],
+                text: 'Email/Password Sign Up',
+              ),
+              CustomButton(
+                onTap: () {
+                  Navigator.pushNamed(context, EmailPasswordLogin.routeName);
+                },
+                text: 'Email/Password Login',
+              ),
+              CustomButton(
+                  onTap: () {
+                    Navigator.pushNamed(context, PhoneScreen.routeName);
+                  },
+                  text: 'Phone Sign In'),
+              CustomButton(
+                onTap: () {},
+                text: 'Google Sign In',
+              ),
+              CustomButton(
+                onTap: () {},
+                text: 'Facebook Sign In',
+              ),
+              CustomButton(
+                onTap: () {},
+                text: 'Anonymous Sign In',
+              ),
+            ],
+          ),
         ),
       ),
     );
